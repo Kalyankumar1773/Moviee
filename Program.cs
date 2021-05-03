cf66e210aef2e0ba1e07c8d8715389609a875dfe
Kalyan Naani <82111056+Kalyankumar1773@users.noreply.github.com>
2021-05-03 20:29:52 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MissingNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Missingnumber();
+        }
+        public static void Missingnumber()
+        {
+            Console.WriteLine("Enter a Number:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            string input = Console.ReadLine();
+            int result = input.Split(' ').Select(int.Parse).Sum();
+            int total = n * (n + 1) / 2;
+            Console.WriteLine(total - result);
+            Console.ReadLine();
+        }
+      
+    }
+}
